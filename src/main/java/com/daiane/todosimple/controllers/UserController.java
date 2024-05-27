6aca3a6e3042bde6cd12b1da56fedfe5c18c717b
DaianePorcena <daiporcenna@gmail.com>
2024-05-27 09:23:39 -0300

    ♻ refactor: refatora a classe UserController

--- src/main/java/com/daiane/todosimple/controllers/UserController.java
+++ src/main/java/com/daiane/todosimple/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,9 +53,10 @@ public class UserController {
     return ResponseEntity.noContent().build();
   }
 
-  @PostMapping("/{id}")
+  @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     this.userService.delete(id);
     return ResponseEntity.noContent().build();
   }
+
 }
