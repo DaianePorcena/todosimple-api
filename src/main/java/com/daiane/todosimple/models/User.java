49d14a0de04aa372f586f3eb175d46773f7da15b
DaianePorcena <daiporcenna@gmail.com>
2024-05-27 10:44:12 -0300

    ♻ refactor: refatora as entidades Task e User

--- src/main/java/com/daiane/todosimple/models/User.java
+++ src/main/java/com/daiane/todosimple/models/User.java
@@ -2,9 +2,6 @@ package com.daiane.todosimple.models;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -17,9 +14,19 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class User {
   public interface CreateUser {
   }
@@ -48,74 +55,7 @@ public class User {
   private String password;
 
   @OneToMany(mappedBy = "user")
+  @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private List<Task> tasks = new ArrayList<Task>();
-
-  public User() {
-  }
-
-  public User(Long id, String username, String password) {
-    this.id = id;
-    this.username = username;
-    this.password = password;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  @JsonIgnore
-  public List<Task> getTasks() {
-    return tasks;
-  }
-
-  public void setTasks(List<Task> tasks) {
-    this.tasks = tasks;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    User other = (User) obj;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    return Objects.equals(id, other.id) && Objects.equals(username, other.username)
-        && Objects.equals(password, other.password);
-  }
 
 }
