4f87533b5acb58421a813c90665f78c7d91d43b1
DaianePorcena <daiporcenna@gmail.com>
2024-05-29 08:49:08 -0300

    ✨ feat: Adiciona método para buscar usuário por nome de usuário

--- src/main/java/com/daiane/todosimple/repositories/UserRepository.java
+++ src/main/java/com/daiane/todosimple/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.daiane.todosimple.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.daiane.todosimple.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+  @Transactional(readOnly = true)
+  User finnByUserName(String username);
 }
